4c3e462b46b8907f8fc7a75a242c01780bb9ad6b
Joseph Walker <joseph.walker@outlook.com>
2018-03-23 15:22:38 -0500

    Deleted references to old deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
