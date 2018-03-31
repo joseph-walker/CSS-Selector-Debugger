2b9a323749c6d419e688e6aa461e81cf4b22b5d8
Joseph Walker <joseph.walker@outlook.com>
2018-03-31 12:39:29 -0500

    Added a dope little badge to the extension icon to show if it's on or off.

--- src/worker.ts
+++ src/worker.ts
@@ -1,11 +1,14 @@
 // This event worker will listen for state changes from the extension panel,
 // as well as load the application state and render it on DOM load of tab change.
 
+import { ap } from 'ramda';
+
 import { Model } from './data/appModel';
 import { stateFromStorage$ } from './data/storage';
 import { drawSelectors } from "./drawSelectors";
 
 const drawSelectorsFromState = (state: Model) => drawSelectors(state.selectors, state.appConfiguration.enabled);
+const updateBadge = (state: Model) => chrome.browserAction.setBadgeText({ text: state.appConfiguration.enabled ? 'ON' : '' });
 
 function loadStateAndDrawSelectors() {
     return stateFromStorage$.subscribe(drawSelectorsFromState);
@@ -13,4 +16,4 @@ function loadStateAndDrawSelectors() {
 
 chrome.webNavigation.onDOMContentLoaded.addListener(loadStateAndDrawSelectors);
 chrome.tabs.onActivated.addListener(loadStateAndDrawSelectors);
-chrome.runtime.onMessage.addListener(drawSelectorsFromState);
+chrome.runtime.onMessage.addListener(ap([drawSelectorsFromState, updateBadge]));
